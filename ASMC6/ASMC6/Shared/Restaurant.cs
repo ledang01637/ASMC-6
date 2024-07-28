5cb47ec8ce35938a537e8dfdaec01913d523b659
metmoiquatroi <vinhlqpc05646@fpt.edu.vn>
2024-07-28 20:31:03 +0700

    xong dang ky ban hàng vs hien thong tin user chua bat loi duoc voi hinh anh không luu dc vào wwwroot

--- ASMC6/ASMC6/Shared/Restaurant.cs
+++ ASMC6/ASMC6/Shared/Restaurant.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,16 @@ namespace ASMC6.Shared
 {
     public class Restaurant
     {
+        [Key]
         public int RestaurantId { get; set; }
         public int UserId {  get; set; }
+        [Required(ErrorMessage = "Không được bỏ trống thông tin")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Không được bỏ trống thông tin")]
         public string Image { get; set; }
+        [Required(ErrorMessage = "Không được bỏ trống thông tin")]
         public string Address { get; set; }
+        [Required(ErrorMessage = "Không được bỏ trống thông tin")]
         public DateTime OpeningHours { get; set; }
         public bool IsDelete { get; set; }
 
