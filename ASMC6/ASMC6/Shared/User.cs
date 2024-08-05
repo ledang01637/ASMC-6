5ebb4e6eb6dc2320938ace9a5a6561dfad0211c4
triet124 <trantantriet18112004@gmail.com>
2024-08-05 12:46:52 +0700

    Validate Register complete
    hoàn thành bắt lỗi đăng ký người dùng

--- ASMC6/ASMC6/Shared/User.cs
+++ ASMC6/ASMC6/Shared/User.cs
@@ -12,10 +12,26 @@ namespace ASMC6.Shared
         [Key]
         public int UserId { get; set; }
         public int RoleId {  get; set; }
+
+        [Required(ErrorMessage = "Tên là bắt buộc")]
+        [DataType(DataType.Text, ErrorMessage = "Không đúng định dạng dữ liệu vui lòng nhập lại")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email là bắt buộc")]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]{3,}\.[a-zA-Z0-9-.]+$", ErrorMessage = "Email không hợp lệ")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password là bắt buộc")]
+        [DataType(DataType.Password, ErrorMessage = "Không đúng định dạng dữ liệu vui lòng nhập lại")]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "Phone là bắt buộc")]
+        [MaxLength(10, ErrorMessage = "Số điện thoại sai định dạng vui lòng nhập lại")]
+        [MinLength(10, ErrorMessage = "Số điện thoại sai định dạng vui lòng nhập lại")]
         public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Address là bắt buộc")]
+        [DataType(DataType.Text, ErrorMessage = "Không đúng định dạng dữ liệu vui lòng nhập lại")]
         public string Address { get; set; }
         public bool IsDelete { get; set; }
 
