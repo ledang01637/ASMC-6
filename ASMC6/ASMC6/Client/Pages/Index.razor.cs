2f4a0f0dec4211e560fdd289c023886109d46a02
DangLH <ledang01637@gmail.com>
2024-08-11 14:40:38 +0700

    Done fix js

--- ASMC6/ASMC6/Client/Pages/Index.razor.cs
+++ ASMC6/ASMC6/Client/Pages/Index.razor.cs
@@ -7,6 +7,7 @@ using System.Net.Http.Json;
 using System.Linq;
 using ASMC6.Shared;
 using System.Text.Json;
+using ASMC6.Client.Session;
 
 namespace ASMC6.Client.Pages
 {
@@ -15,10 +16,11 @@ namespace ASMC6.Client.Pages
         private List<ASMC6.Shared.Restaurant> listRest = new List<ASMC6.Shared.Restaurant>();
         private List<ASMC6.Shared.Product> listProd = new List<ASMC6.Shared.Product>();
         private List<ASMC6.Shared.Menu> listMenu = new List<ASMC6.Shared.Menu>();
-
+        private ASMC6.Shared.Restaurant restaurant = new ASMC6.Shared.Restaurant();
         protected override async Task OnInitializedAsync()
         {
             await Load();
+            StateHasChanged();
         }
 
         private async Task Load()
@@ -28,6 +30,13 @@ namespace ASMC6.Client.Pages
                 listProd = await httpClient.GetFromJsonAsync<List<ASMC6.Shared.Product>>("api/Product/GetProducts");
                 listRest = await httpClient.GetFromJsonAsync<List<ASMC6.Shared.Restaurant>>("api/Restaurant/GetRestaurants");
                 listMenu = await httpClient.GetFromJsonAsync<List<ASMC6.Shared.Menu>>("api/Menu/GetMenus");
+                if (SUser.User != null)
+                {
+                    restaurant = listRest.FirstOrDefault(r => r.UserId == SUser.User.UserId);
+                    var listRestLogin = listRest.Where(m => m.RestaurantId != restaurant.RestaurantId).ToList();
+                    var menuForRest = listMenu.Where(menu => listRestLogin.Any(lr => lr.RestaurantId == menu.RestaurantId)).ToList();
+                    listProd = listProd.Where(p => menuForRest.Any(mr => mr.MenuId == p.MenuId)).ToList();
+                }
             }
             catch (Exception ex)
             {
